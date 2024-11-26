e4d4af93598ecb9a446405f1437d5c1a3548da18
RaulRangelxD <raulrangel1230@gmail.com>
2024-11-26 14:50:02 -0400

    adapted for auto rebuilt

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
 
 dotenv.config()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
