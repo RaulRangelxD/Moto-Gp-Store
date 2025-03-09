364af611932834c4aacd98d1469f49b6d0660920
RaulRangelxD <raulrangel1230@gmail.com>
2025-03-09 11:12:39 -0400

    add env variable for server

--- client/src/api/example.tsx
+++ client/src/api/example.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
+const API_URL = process.env.EXPO_PUBLIC_API_URL + '/examples'
+
 export const getAllExamples = async () => {
-  const response = await axios.get('http://localhost:3000/examples')
+  const response = await axios.get(API_URL)
   return response.data.data
 }
